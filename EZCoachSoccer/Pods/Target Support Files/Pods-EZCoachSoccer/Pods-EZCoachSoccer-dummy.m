41eb44bba19813c62f59e869479624176f73688d
Daniel Mackay <danielmackay1@hotmail.com>
2017-05-25 19:50:57 -0400

    comments

---
+++ EZCoachSoccer/Pods/Target Support Files/Pods-EZCoachSoccer/Pods-EZCoachSoccer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EZCoachSoccer : NSObject
+@end
+@implementation PodsDummy_Pods_EZCoachSoccer
+@end
---
+++ EZCoachSoccer/Pods/Target Support Files/Pods-EZCoachSoccerTests/Pods-EZCoachSoccerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EZCoachSoccerTests : NSObject
+@end
+@implementation PodsDummy_Pods_EZCoachSoccerTests
+@end
