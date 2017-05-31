321443016f8517e58ae99609ccda02402d7d223c
Daniel Mackay <danielmackay1@hotmail.com>
2017-05-31 14:04:03 -0400

    added animation for formations 11

--- EZCoachSoccer/EZCoachSoccer/ElevenVElevenInterface.swift
+++ EZCoachSoccer/EZCoachSoccer/ElevenVElevenInterface.swift
@@ -17,6 +17,67 @@ public class ElevenVElevenInterface: SKScene{
     var gk11, lcb11, rcb11, rb11,lb11,rcm11,cm11,lcm11,rf11,lf11,str11,ball11 : SKSpriteNode?
     var opgk11, oplcb11, oprcb11, oprb11, oplb11,oprcm11,opcm11,oplcm11,oprf11,oplf11,opstr11 : SKSpriteNode?
     
+    // formation animations
+    // 4-3-3
+    var STR433 = SKAction.move(to: CGPoint(x: 96.203, y: -2.689), duration: 1)
+    var rf433 = SKAction.move(to: CGPoint(x: 96.203, y: 222.311), duration: 1)
+    var lf433 = SKAction.move(to: CGPoint(x: 96.203, y: -251.763), duration: 1)
+    var rcm433 = SKAction.move(to: CGPoint(x: 273.203, y: -151.763), duration: 1)
+    var lcm433 = SKAction.move(to: CGPoint(x: 273.203, y: 150.311), duration: 1)
+    var cm433 = SKAction.move(to: CGPoint(x: 273.203, y: -2.689), duration: 1)
+    var lb433 = SKAction.move(to: CGPoint(x: 405.203, y: -296.689), duration: 1)
+    var rb433 = SKAction.move(to: CGPoint(x: 390.203, y: 288.311), duration: 1)
+    var rcb433 = SKAction.move(to: CGPoint(x: 429.25, y: 98.786), duration: 1)
+    var lcb433 = SKAction.move(to: CGPoint(x: 429.25, y: -101.214), duration: 1)
+    
+    // 4-4-2
+    var lb442 = SKAction.move(to: CGPoint(x: 405.203, y: -296.689), duration: 1)
+    var rb442 = SKAction.move(to: CGPoint(x: 390.203, y: 288.311), duration: 1)
+    var rcb442 = SKAction.move(to: CGPoint(x: 429.25, y: 98.786), duration: 1)
+    var lcb442 = SKAction.move(to: CGPoint(x: 429.25, y: -101.214), duration: 1)
+    var rf442 = SKAction.move(to: CGPoint(x: 232.175, y: 269.69), duration: 1)
+    var lcm442 = SKAction.move(to: CGPoint(x: 273.203, y: 97.311), duration: 1)
+    var cm442 = SKAction.move(to: CGPoint(x: 273.203, y: -77.689), duration: 1)
+    var rcm442 = SKAction.move(to: CGPoint(x: 243.005, y: -251.763), duration: 1)
+    var str442 = SKAction.move(to: CGPoint(x: 92.203, y: 97.311), duration: 1)
+    var lf442 = SKAction.move(to: CGPoint(x: 93.203, y: -62.966), duration: 1)
+    
+    // 4-2-3-1
+    var lb4231 = SKAction.move(to: CGPoint(x: 405.203, y: -296.689), duration: 1)
+    var rb4231 = SKAction.move(to: CGPoint(x: 390.203, y: 288.311), duration: 1)
+    var rcb4231 = SKAction.move(to: CGPoint(x: 429.25, y: 98.786), duration: 1)
+    var lcb4231 = SKAction.move(to: CGPoint(x: 429.25, y: -101.214), duration: 1)
+    var cm4231 = SKAction.move(to: CGPoint(x: 302.203, y: -77.689), duration: 1)
+    var lcm4231 = SKAction.move(to: CGPoint(x: 302.203, y: 72.311), duration: 1)
+    var rf4231 = SKAction.move(to: CGPoint(x: 174.547, y: -101.214), duration: 1)
+    var lf4231 = SKAction.move(to: CGPoint(x: 175.474, y: -2.689), duration: 1)
+    var rcm4231 = SKAction.move(to: CGPoint(x: 175.474, y: -101.214), duration: 1)
+    var str4231 = SKAction.move(to: CGPoint(x: 75.474, y: -2.689), duration: 1)
+    
+    // 3-4-3
+    var rb343 = SKAction.move(to: CGPoint(x: 429.25, y: 198.786), duration: 1)
+    var rcb343 = SKAction.move(to: CGPoint(x: 429.25, y: -15.739), duration: 1)
+    var lcb343 = SKAction.move(to: CGPoint(x: 429.25, y: -215.739), duration: 1)
+    var lf343 = SKAction.move(to: CGPoint(x: 266.727, y: 298.786), duration: 1)
+    var lcm343 = SKAction.move(to: CGPoint(x: 266.727, y: 72.311), duration: 1)
+    var cm343 = SKAction.move(to: CGPoint(x: 266.727, y: -102.689), duration: 1)
+    var lb343 = SKAction.move(to: CGPoint(x: 266.727, y: 315.739), duration: 1)
+    var str343 = SKAction.move(to: CGPoint(x: 75.474, y: -2.689), duration: 1)
+    var rcm343 = SKAction.move(to: CGPoint(x: 75.474, y: -181.876), duration: 1)
+    var rf343 = SKAction.move(to: CGPoint(x: 75.474, y: 172.311), duration: 1)
+    
+    // 5-3-2
+    var rb532 = SKAction.move(to: CGPoint(x: 429.25, y: 198.786), duration: 1)
+    var rcb532 = SKAction.move(to: CGPoint(x: 429.25, y: -15.739), duration: 1)
+    var lcb532 = SKAction.move(to: CGPoint(x: 429.25, y: -215.739), duration: 1)
+    var lb532 = SKAction.move(to: CGPoint(x: 350.551, y: -315.739), duration: 1)
+    var lcm532 = SKAction.move(to: CGPoint(x: 264.828, y: 122.311), duration: 1)
+    var cm532 = SKAction.move(to: CGPoint(x: 264.828, y: -15.739), duration: 1)
+    var rcm532 = SKAction.move(to: CGPoint(x: 264.828, y: -170.077), duration: 1)
+    var str532 = SKAction.move(to: CGPoint(x: 94.843, y: -81.876), duration: 1)
+    var lf532 = SKAction.move(to: CGPoint(x: 350.551, y: 297.786), duration: 1)
+    var rf532 = SKAction.move(to: CGPoint(x: 94.834, y: 50.311), duration: 1)
+    
     public override func didMove(to view: SKView) {
         ball11 = childNode(withName: "ball11") as? SKSpriteNode!
         gk11 = childNode(withName: "gk11") as? SKSpriteNode!
@@ -97,5 +158,70 @@ public class ElevenVElevenInterface: SKScene{
                 }
             }
         }
+    }
+    
+    func form433(){
+        lcb11?.run(lcb433)
+        rcb11?.run(rcb433)
+        rb11?.run(rb433)
+        lb11?.run(lb433)
+        cm11?.run(cm433)
+        rcm11?.run(rcm433)
+        lcm11?.run(lcm433)
+        lf11?.run(lf433)
+        rf11?.run(rf433)
+        str11?.run(STR433)
+    }
+    
+    func form442(){
+        lcb11?.run(lcb442)
+        rcb11?.run(rcb442)
+        lb11?.run(lb442)
+        rb11?.run(rb442)
+        cm11?.run(cm442)
+        rcm11?.run(rcm442)
+        lcm11?.run(lcm442)
+        lf11?.run(lf442)
+        rf11?.run(rf442)
+        str11?.run(str442)
+    }
+    
+    func form4231(){
+        lcb11?.run(lcb4231)
+        rcb11?.run(rcb4231)
+        rb11?.run(rb4231)
+        lb11?.run(lb4231)
+        cm11?.run(cm4231)
+        rcm11?.run(rcm4231)
+        lcm11?.run(lcm4231)
+        rf11?.run(rf4231)
+        lf11?.run(lf4231)
+        str11?.run(str4231)
+    }
+    
+    func form343(){
+        lcb11?.run(lcb343)
+        rcb11?.run(rcb343)
+        lb11?.run(lb343)
+        rb11?.run(rb343)
+        cm11?.run(cm343)
+        rcm11?.run(rcm343)
+        lcm11?.run(lcm343)
+        rf11?.run(rf343)
+        lf11?.run(lf343)
+        str11?.run(str343)
+    }
+    
+    func form532(){
+        lcb11?.run(lcb532)
+        rcb11?.run(rcb532)
+        rb11?.run(rb532)
+        lb11?.run(lb532)
+        cm11?.run(cm532)
+        lcm11?.run(lcm532)
+        rcm11?.run(rcm532)
+        rf11?.run(rf532)
+        lf11?.run(lf532)
+        str11?.run(str532)
     }
 }
