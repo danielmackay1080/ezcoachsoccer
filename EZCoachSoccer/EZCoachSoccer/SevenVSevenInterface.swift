f5b8b977265b5963b98b8b94ee237aabf60d5395
Daniel Mackay <danielmackay1@hotmail.com>
2017-05-31 18:42:29 -0400

    added formations 7

--- EZCoachSoccer/EZCoachSoccer/SevenVSevenInterface.swift
+++ EZCoachSoccer/EZCoachSoccer/SevenVSevenInterface.swift
@@ -32,6 +32,50 @@ public class SevenVSevenInterface : SKScene{
     
     var ball7 : SKSpriteNode?
     
+    // formations
+    // 3-3
+    var lcb33 = SKAction.move(to: CGPoint(x: 449.765, y: -233.665), duration: 1)
+    var rcb33 = SKAction.move(to: CGPoint(x: 449.765, y: 217.601), duration: 1)
+    var cm33 = SKAction.move(to: CGPoint(x: 412.012, y: -2.845), duration: 1)
+    var rm33 = SKAction.move(to: CGPoint(x: 194.924, y: 217.601), duration: 1)
+    var lm33 = SKAction.move(to: CGPoint(x: 194.924, y: -233.665), duration: 1)
+    var st33 = SKAction.move(to: CGPoint(x: 91.331, y: -2.845), duration: 1)
+    
+    //3-1-2
+    var lcb312 = SKAction.move(to: CGPoint(x: 449.765, y: -233.665), duration: 1)
+    var rcb312 = SKAction.move(to: CGPoint(x: 449.765, y: 217.601), duration: 1)
+    var cm312 = SKAction.move(to: CGPoint(x: 412.012, y: -2.845), duration: 1)
+    var rm312 = SKAction.move(to: CGPoint(x: 103.939, y: 127.155), duration: 1)
+    var st312 = SKAction.move(to: CGPoint(x: 103.939, y: -119.71), duration: 1)
+    var lm312 = SKAction.move(to: CGPoint(x: 250.425, y: -2.845), duration: 1)
+    
+    // 2-3-1
+    var lcb231 = SKAction.move(to: CGPoint(x: 449.765, y: -148.734), duration: 1)
+    var rcb231 = SKAction.move(to: CGPoint(x: 449.765, y: 153.196), duration: 1)
+    var cm231 = SKAction.move(to: CGPoint(x: 294.788, y: -2.845), duration: 1)
+    var rm231 = SKAction.move(to: CGPoint(x: 250.425, y: 227.747), duration: 1)
+    var lm231 = SKAction.move(to: CGPoint(x: 250.425, y: -262.845), duration: 1)
+    var st231 = SKAction.move(to: CGPoint(x: 120.425, y: -2.845), duration: 1)
+    
+    //2-1-2-1
+    var lcb2121 = SKAction.move(to: CGPoint(x: 449.765, y: -148.734), duration: 1)
+    var rcb2121 = SKAction.move(to: CGPoint(x: 449.765, y: 153.196), duration: 1)
+    var cm2121 = SKAction.move(to: CGPoint(x: 345.146, y: -1.204), duration: 1)
+    var rm2121 = SKAction.move(to: CGPoint(x: 197.707, y: 177.751), duration: 1)
+    var lm2121 = SKAction.move(to: CGPoint(x: 197.707, y: -184.983), duration: 1)
+    var st2121 = SKAction.move(to: CGPoint(x: 90.785, y: -1.204), duration: 1)
+    
+    //1-3-2
+    var lcb132 = SKAction.move(to: CGPoint(x: 439.309, y: -2.845), duration: 1)
+    var rcb132 = SKAction.move(to: CGPoint(x: 288.969, y: 227.747), duration: 1)
+    var cm132 = SKAction.move(to: CGPoint(x: 288.969, y: -2.845), duration: 1)
+    var lm132 = SKAction.move(to: CGPoint(x: 288.969, y: -248.852), duration: 1)
+    var rm132 = SKAction.move(to: CGPoint(x:90.785, y: 128.796), duration: 1)
+    var st132 = SKAction.move(to: CGPoint(x: 105.568, y: -118.852), duration: 1)
+    
+    
+    
+    
     public override func didMove(to view: SKView) {
         
         gk7 = childNode(withName: "gk7") as? SKSpriteNode!
@@ -90,6 +134,51 @@ public class SevenVSevenInterface : SKScene{
                 }
             }
         }
+    }
+    
+    func form33(){
+        lcb7?.run(lcb33)
+        rcb7?.run(rcb33)
+        rm7?.run(rm33)
+        lm7?.run(lm33)
+        cm7?.run(cm33)
+        str7?.run(st33)
+    }
+    
+    func form312(){
+        lcb7?.run(lcb312)
+        rcb7?.run(rcb312)
+        rm7?.run(rm312)
+        lm7?.run(lm312)
+        cm7?.run(cm312)
+        str7?.run(st312)
+    }
+    
+    func form231(){
+        lcb7?.run(lcb231)
+        rcb7?.run(rcb231)
+        cm7?.run(cm231)
+        lm7?.run(lm231)
+        rm7?.run(rm231)
+        str7?.run(st231)
+    }
+    
+    func form2121(){
+        lcb7?.run(lcb2121)
+        rcb7?.run(rcb2121)
+        cm7?.run(cm2121)
+        lm7?.run(lm2121)
+        rm7?.run(rm2121)
+        str7?.run(st2121)
+    }
+    
+    func form132(){
+        lcb7?.run(lcb132)
+        rcb7?.run(rcb132)
+        cm7?.run(cm132)
+        lm7?.run(lm132)
+        rm7?.run(rm132)
+        str7?.run(st132)
     }
     
 }
